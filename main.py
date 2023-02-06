1583a46631b127cddb9ee60db4584a942ffa7abe
Austin Kelsay <austinkelsay@yahoo.com>
2023-02-06 15:55:13 -0600

    Initial

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+nostr_data = {
+    "names": {
+        "bob": "b0635d6a9851d3aed0cd6c495b282167acf761729078d975fc341b22650b07b9"
+    },
+    "relays": {
+        "b0635d6a9851d3aed0cd6c495b282167acf761729078d975fc341b22650b07b9": ["wss://relay.example.com", "wss://relay2.example.com"]
+    }
+}
+
+
+@app.route('/.well-known/nostr.json')
+def nostr_json():
+    name = request.args.get("name")
+    print(type(name))
+    if name not in nostr_data["names"]:
+        return "", 404
+    return jsonify(nostr_data)
+
+
+if __name__ == '__main__':
+    app.run()
