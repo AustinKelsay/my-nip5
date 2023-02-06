e0d0165273cae36886bbc575f1aa784ff1f90e18
Austin Kelsay <austinkelsay@yahoo.com>
2023-02-06 16:39:05 -0600

    Taking out relays from json for now

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 nostr_data = {
     "names": {
         "devplebbitcoin": "022522f88059c684e200cbfec9f045eb4c9ed57c158e0294cd01eb81de62e211"
-    },
-    "relays": {
-        "022522f88059c684e200cbfec9f045eb4c9ed57c158e0294cd01eb81de62e211": ["wss://nostr.mom", "wss://no-str.org"]
     }
 }
 
@@ -15,7 +12,6 @@ nostr_data = {
 @app.route('/.well-known/nostr.json')
 def nostr_json():
     name = request.args.get("name")
-    print(type(name))
     if name not in nostr_data["names"]:
         return "", 404
     return jsonify(nostr_data)
