ddccea57d48b876f94f184cd45977893170780b9
Austin Kelsay <austinkelsay@yahoo.com>
2023-02-08 14:03:14 -0600

    Updated and tested successfully without relays property

--- api/index.py
+++ api/index.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 nostr_data = {
     "names": {
         "<YOUR NOSTR ALIAS>": "<YOUR NOSTR PUBLIC KEY IN HEX FORMAT>"
-    },
-    "relays": {
-        "<YOUR NOSTR PUBLIC KEY IN HEX FORMAT>": ["wss://relay.example.com", "wss://relay2.example.com"]
     }
 }
 
@@ -19,7 +16,7 @@ def nostr_json():
     if public_key is None:
         return "", 404
 
-    response_data = {"relays": nostr_data["relays"].get(public_key, [])}
+    response_data = {"names": {name: public_key}}
     response = jsonify(response_data)
     response.headers["Content-Type"] = "application/json"
     response.headers.add("Access-Control-Allow-Origin", "*")
